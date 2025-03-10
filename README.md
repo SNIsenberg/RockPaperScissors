e1507c4bb4bd489c91d666b3bb609e18516e5ea3
Sara Nechama Isenberg <snisenberg@wits.edu>
2025-03-09 22:34:46 -0400

    Adding the rock-paper-scissors project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RockPaperScissors
+
+### CIS 365: Web Programming
+### Sara Nechama Isenberg
+#### March 10
+
+---
+
+This is a Rock-Paper-Scissor game that allows the user to play against the computer using clickable images or their keyboard. Makes use of Javascript event handling.
---
+++ scripts.js
@@ -0,0 +1,66 @@
+document.getElementById("rockBtn").addEventListener('click', function() {calculateResults("rock", myChoice());});
+document.getElementById("paperBtn").addEventListener('click', function() {calculateResults("paper", myChoice());});
+document.getElementById("scissorsBtn").addEventListener('click', function() {calculateResults("scissors", myChoice())});
+document.getElementById("again").addEventListener('click', function() {reset()});
+document.addEventListener('keydown', function(event) {useKeyboard(event.key)})
+
+function myChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    if (choice === 1) {
+        choice = "rock";
+    }
+    else if (choice === 2) {
+        choice = "paper";
+    }
+    else {
+        choice = "scissors";
+    }
+    document.getElementById("computerChoice").innerHTML = choice;
+    return choice;
+}
+
+function calculateResults(userChoice, computerChoice) {
+    document.getElementById("userChoice").innerHTML = userChoice;
+    let chosens = userChoice + "-" + computerChoice;
+    console.log(chosens);
+    switch (chosens) {
+        case ("rock-rock"):
+        case ("paper-paper"):
+        case ("scissors-scissors"):
+            document.getElementById("winner").innerHTML = "It is a tie!";
+            break;
+        case ("rock-scissors"):
+        case ("paper-rock"):
+        case ("scissors-paper"):
+            document.getElementById("winner").innerHTML = "You win!";
+            break;
+        case ("rock-paper"):
+        case ("paper-scissors"):
+        case ("scissors-rock"):
+            document.getElementById("winner").innerHTML = "Computer wins!";
+            break;
+        default:
+            document.getElementById("winner").innerHTML = "Something went wrong, press play again";
+    }
+    document.getElementById("imgContainer").style.display = "none";
+    document.getElementById("results").style.display = "block";
+    document.getElementById("again").style.display = "block";
+}
+
+function reset() {
+    document.getElementById("results").style.display = "none";
+    document.getElementById("imgContainer").style.display = "grid";
+    document.getElementById("again").style.display = "none";
+}
+
+function useKeyboard(key) {
+    if (key == 'r') {
+        calculateResults("rock", myChoice());
+    }
+    else if (key == 'p') {
+        calculateResults("paper", myChoice());
+    }
+    else if (key == 's') {
+        calculateResults("scissors", myChoice());
+    }
+}
